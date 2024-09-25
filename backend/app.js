d210528e69c0651308e4c99754a6567ce513aca6
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2024-09-25 13:31:15 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the Backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
 
