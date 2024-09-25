d482989c41c9e3a6b7e9a86123fd0c67a8d568fc
Karim Osman <60973591+karimosman89@users.noreply.github.com>
2024-09-25 14:36:17 +0200

    Create index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Adjust the path if necessary
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root') // This should match the id in your index.html
+);
